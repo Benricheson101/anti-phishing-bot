2e5ffd6f15d4a7d87579b2dfc37c27be457e98b7
Nick <harbinger.nick.a@gmail.com>
2021-10-18 16:47:24 -0600

    guildMemberCreate handler

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,20 +2,24 @@ import {PrismaClient} from '@prisma/client';
 import {DomainStore} from './domains';
 import {ExemptionStore} from './exemptions';
 import {GuildConfigStore} from './guildConfigs';
+import {MutedStore} from './muted';
 
 export * from './domains';
 export * from './exemptions';
 export * from './guildConfigs';
+export * from './muted';
 
 // TODO: implement some sort of caching
 export class Database {
   domains: DomainStore;
   guildConfigs: GuildConfigStore;
   exemptions: ExemptionStore;
+  muted: MutedStore;
 
   constructor(prisma: PrismaClient) {
     this.domains = new DomainStore(prisma);
     this.guildConfigs = new GuildConfigStore(prisma);
     this.exemptions = new ExemptionStore(prisma);
+    this.muted = new MutedStore(prisma);
   }
 }
